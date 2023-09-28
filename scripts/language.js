be1010bbe40744ba810652600bb78a9da7300fdd
Shyngys001 <shyngyskhairolla04@mail.ru>
2023-09-29 01:41:21 +0600

    hhoh

--- scripts/language.js
+++ scripts/language.js
@@ -4,18 +4,20 @@ let language_ru = document.querySelector(".language-ru");
 
 
 
-let second_1 = document.querySelector("#second_1");
-let second_2 = document.querySelector("#second_2");
-let about_mission = document.querySelector("#about_mission");
-let about_protect = document.querySelector("#about_protect");
-let about_realize = document.querySelector("#about_realize");
-let about_there = document.querySelector("#about_there");
-let about_assist = document.querySelector("#about_assist");
-let about_customers = document.querySelector("#about_customers");
-let about_appreciate = document.querySelector("#about_appreciate");
-let about_each = document.querySelector("#about_each");
 
 language_ru.addEventListener("click", () => {
+    link_product.textContent = "Наши продукты и услуги";
+    link_history.textContent = "Наша история";
+    link_certificate.textContent = "Сертификаты";
+    link_contact.textContent = "Контакты";
+    link_about.textContent = "О нас";
+
+    menu_link_product.textContent = "Наши продукты и услуги";
+    menu_link_history.textContent = "Наша история";
+    menu_link_certificate.textContent = "Сертификаты";
+    menu_link_contact.textContent = "Контакты";
+    menu_link_about.textContent = "О нас";
+
     second_1.textContent = "Актауский завод по производству труб";
     second_2.textContent = "Актауский завод по производству труб специализируется при производстве и ремонте труб мы предлагаем экономически эффективные решения, которые сводят к минимуму время простоя и защищают трубопроводы от коррозии и износа.";
     about_mission.textContent = "Миссия нашей компании";
@@ -26,5 +28,7 @@ language_ru.addEventListener("click", () => {
     about_customers.textContent = "Заказчикам в повышении эффективности оборудования, экономии ресурсов и минимизации потерь при добыче нефти";
     about_appreciate.textContent = "Мы ценим";
     about_each.textContent = "Каждых наших клиентов и их оборудование";
-
 });
+
+
+
